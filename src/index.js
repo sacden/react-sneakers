91e0fc7aa52439374c0f9f3c3bcf8e27c502910d
sacden <sacden2009@gmail.com>
2022-07-15 15:28:22 +0200

    fix: router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import "macro-css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
